bdfbd7b1651c9a285d80c85719de5984f5e07172
xiaoqb <xiaoqb@foxmail.com>
2023-02-16 13:30:04 +0800

    feat: Initialize the project with start.spring.io

---
+++ single/src/main/java/top/xiaoboey/practice/spring/boot3/single/SingleApplication.java
@@ -0,0 +1,13 @@
+package top.xiaoboey.practice.spring.boot3.single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleApplication.class, args);
+	}
+
+}
---
+++ single/src/test/java/top/xiaoboey/practice/spring/boot3/single/SingleApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xiaoboey.practice.spring.boot3.single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
